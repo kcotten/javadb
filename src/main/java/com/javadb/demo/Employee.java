dd8bd47c7983f80e2dac8c988da4458b9855bb40
kcotten <kris.cotten@gmail.com>
2021-09-25 10:30:45 -0700

    Init

---
+++ src/main/java/com/javadb/demo/Employee.java
@@ -0,0 +1,25 @@
+package com.javadb.demo;
+
+import org.springframework.data.annotation.Id;
+
+public class Employee {
+    @Id
+    public String id;
+  
+    public String firstName;
+    public String lastName;
+  
+    public Employee() {}
+  
+    public Employee(String firstName, String lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    @Override
+    public String toString() {
+      return String.format(
+          "Employee[id=%s, firstName='%s', lastName='%s']",
+          id, firstName, lastName);
+    }
+}
---
+++ src/main/java/com/javadb/demo/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package com.javadb.demo;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface EmployeeRepo extends MongoRepository<Employee, String> {
+    public Employee findByFirstName(String firstName);
+    public List<Employee> findByLastName(String lastName);
+}
