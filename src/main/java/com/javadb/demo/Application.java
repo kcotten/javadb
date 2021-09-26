b85fccba287169473edf90cb2d49a165821f8abb
kcotten <kris.cotten@gmail.com>
2021-09-26 11:05:48 -0700

    Remove comments

--- src/main/java/com/javadb/demo/Application.java
+++ src/main/java/com/javadb/demo/Application.java
@@ -1,20 +1,11 @@
 package com.javadb.demo;
 
-// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		//@PropertySource String SPRING_DATA_MONGODB_HOST = System.getenv("SPRING_DATA_MONGODB_HOST");
-		
+	public static void main(String[] args) {		
 		SpringApplication.run(Application.class, args);
 	}
-
-	// @Override
-  	// public void run(String... args) throws Exception {
-
-  	// }
 }
